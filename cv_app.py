62277ae509d0111d66c9937969cea278ba4193ad
Victor Alcantar <victor.alcantar01@gmail.com>
2025-07-26 23:07:26 -0600

    Patentes

---
+++ cv_app.py
@@ -0,0 +1,167 @@
+import streamlit as st
+import data_info
+
+st.set_page_config(
+    page_title="Víctor Alcántar",  # Título de la pestaña del navegador
+    layout="centered"  # o "wide" para más espacio
+)
+
+# --- Barra lateral de navegación ---
+with st.sidebar:
+    st.markdown("## Navegación")
+    st.markdown("[Inicio](#inicio)")
+    st.markdown("[Formación académica](#formacion)")
+    st.markdown("[Antecedentes laborales](#laburo)")
+    st.markdown("[Distinciones](#distincion)")
+    st.markdown('[Reconocimientos académicos](#rec_aca)')
+    st.markdown('[Materias impartidas](#materias)')
+    st.markdown('[Membresías](#membresias)')
+    st.markdown('[Patentes](#patentes)')
+    st.markdown('[Publicaciones en revistas indizadas](#indizadas)')
+
+
+# Sección inicial
+st.markdown('<a id="inicio"></a>', unsafe_allow_html=True)
+col1, col2 = st.columns([1, 3])
+with col1:
+    # Muestra la imagen (ajusta el width al tamaño deseado)
+    st.image("archivos/foto_cv.png", width=120)
+
+with col2:
+    st.markdown("### Víctor Alfonso Alcántar Camarena")
+    st.write("Dr. en Ingeniería Mecánica")
+    st.write("Universidad Politécnica del Bicentenario")
+    st.write("📧 valcantarc@upbicentenario.edu.mx")
+
+st.info(data_info.semblanza)
+st.markdown("---")
+
+
+# Formación académica
+st.markdown('<a id="formacion"></a>', unsafe_allow_html=True)
+st.success('#### Formación académica')
+for item in data_info.formacion:
+    col1, col2 = st.columns([1, 5])
+    with col1:
+        st.markdown(f"{item['periodo']}")  
+    with col2:
+        st.markdown(f"""
+        {item['institucion']}  
+        {item['grado']}  
+        *Tesis:* {item['tesis']}  
+        """)
+    #st.markdown("<br>", unsafe_allow_html=True)
+st.markdown('---')
+
+
+# Antecedentes laborales
+st.markdown('<a id="laburo"></a>', unsafe_allow_html=True)
+st.success('#### Antecedentes laborales')
+for item in data_info.ant_lab:
+    col1, col2 = st.columns([1, 5])
+    with col1:
+        st.markdown(f"{item['periodo']}")
+    with col2:
+        st.markdown(f"""
+        {item['institucion']}
+        {item['actividad']}
+        """)
+    #st.markdown("<br>", unsafe_allow_html=True)
+st.markdown('---')
+
+
+# Distinciones
+st.markdown('<a id="distincion"></a>', unsafe_allow_html=True)
+st.success('#### Distinciones')
+for item in data_info.distincion:
+    col1, col2 = st.columns([1, 5])
+    with col1:
+        st.markdown(f"{item['periodo']}")
+    with col2:
+        st.markdown(f"""
+        {item['distincion']}
+        """)
+    #st.markdown("<br>", unsafe_allow_html=True)
+st.markdown('---')
+
+
+# Reconocimientos académicos
+st.markdown('<a id="rec_aca"></a>', unsafe_allow_html=True)
+st.success('#### Reconocimientos académicos')
+for item in data_info.rec_acad:
+    col1, col2 = st.columns([1, 5])
+    with col1:
+        st.markdown(f"**{item['periodo']}**")
+    with col2:
+        st.markdown(f"""
+        {item['reconocimiento']}  
+        {item['trabajo']}  
+        {item['institucion']}
+        """)
+    #st.markdown("<br>", unsafe_allow_html=True)
+st.markdown('---')
+
+
+# Materias impartidas
+st.markdown('<a id="materias"></a>', unsafe_allow_html=True)
+st.success("#### Materias impartidas")
+for materia in data_info.materias:
+    st.markdown(f"- {materia}")
+st.markdown('---')
+
+
+# Membresías
+st.markdown('<a id="membresias"></a>', unsafe_allow_html=True)
+st.success('#### Membresías')
+for item in data_info.membresia:
+    col1, col2 = st.columns([1, 5])
+    with col1:
+        st.markdown(f"**{item['periodo']}**")
+    with col2:
+        st.markdown(f"""
+        {item['comite']}  
+        {item['registro']}  
+        """)
+    #st.markdown("<br>", unsafe_allow_html=True)
+st.markdown('---')
+
+
+# Patentes
+st.markdown('<a id="patentes"></a>', unsafe_allow_html=True)
+st.success('#### Patentes')
+for item in data_info.patentes:
+    col1, col2 = st.columns([1, 5])
+    with col1:
+        st.markdown(f"**{item['anio']}**")
+    with col2:
+        st.markdown(f"""
+        {item['titulo']}  
+        {item['inventores']}  
+        {item['num_patente']}  
+        {item['pais']}  
+        {item['enlace']}   
+        """)
+    #st.markdown("<br>", unsafe_allow_html=True)
+st.markdown('---')
+
+# Publicaciones en revistas indizadas
+st.markdown('<a id="indizadas"></a>', unsafe_allow_html=True)
+st.success('#### Publicaciones en revistas indizadas')
+for item in data_info.indizadas:
+    col1, col2 = st.columns([1, 5])
+    with col1:
+        st.markdown(f"**{item['anio']}**")
+    with col2:
+        st.markdown(f"""
+        {item['titulo']}  
+        {item['autores']}  
+        {item['journal']}  
+        {item['volumen']}  
+        {item['paginas']}  
+        {item['revista']}  
+        {item['doi']}  
+        {item['impacto']}  
+        """)
+    #st.markdown("<br>", unsafe_allow_html=True)
+st.markdown('---')
+
---
+++ data_info.py
@@ -0,0 +1,235 @@
+semblanza = 'Grado de Dr. en Ingeniería Mecánica por parte de la Universidad de Guanajuato. Mi ' \
+'proyecto de doctorado consistió en la optimización de recipientes a presión para el almacenamiento ' \
+'de hidrógeno como combustible. Para el desarrollo de este trabajo se emplearon métodos de optimización' \
+' heurísticos como los algoritmos genéticos y el templado simulado. Así mismo, he trabajado con redes' \
+' neuronales artificiales aplicadas a la ingeniería. Desde 2015 me desempeño como profesor investigador' \
+' de tiempo completo en la Universidad Politécnica del Bicentenario en la Carrera de Ingeniería en' \
+' Diseño Industrial.'
+
+# Formación académica
+formacion = [
+    {
+        "periodo": "2011-2015",  
+        "institucion": "Universidad de Guanajuato",  
+        "grado": "Doctorado en Ing. Mecánica",  
+        "tesis": "Optimización de recipientes a presión tipo III y IV"
+    },
+    {
+        "periodo": "2009-2011",  
+        "institucion": "Universidad de Guanajuato",  
+        "grado": "Maestría en Ing. Mecánica",  
+        "tesis": "Análisis estructural de tanques a presión tipo III reforzados con fibras"
+    },
+    {
+        "periodo": "2003-2009",  
+        "institucion": "Inst. Tecnológico Superior de Irapuato",  
+        "grado": "Licenciatura en Ing. Electromecánica",  
+        "tesis": "Rediseño de apilador de una línea de producción"
+    }
+]
+
+# Antecedentes laborales
+ant_lab = [
+    {
+        "periodo": "2015-actual",
+        "institucion": "Universidad Politécnica del Bicentenario",
+        "actividad": "Profesor Titular A"
+    },
+    {
+        "periodo": "2012-2014",
+        "institucion": "Universidad de Guanajuato",
+        "actividad": "Profesor por asignatura"
+    },
+    {
+        "periodo": "2007-2008",
+        "institucion": "LAGERMEX, S.A. de C.V.",
+        "actividad": "Auxiliar en departamento de Ingeniería"
+    }
+]
+
+
+# Distinciones
+distincion = [
+    {
+        'periodo': '2022-2026',
+        'distincion': 'Sistema Nacional de Investigadores Nivel 1'
+    },
+    {
+        'periodo': '2018-2020',
+        'distincion':'Reconocimiento a Perfil Deseable'
+    }
+]
+
+
+# Reconocimientos académicos
+rec_acad = [
+    {
+        "periodo": "Ago 2015",
+        "reconocimiento": "Reconocimiento de trabajo Doctoral de grado Laureado",
+        "trabajo": "Tesis: Optimización de recipientes a presión del Tipo III y IV",
+        "institucion": "División de Ingenierías - CIS, Universidad de Guanajuato"
+    },
+    {
+        "periodo": "Mar 2011",
+        "reconocimiento": "Reconocimiento de trabajo de Maestría de grado Laureado",
+        "trabajo": "Tesis: Análisis estructural de tanques a presión del Tipo III reforzados con fibras",
+        "institucion": "División de Ingenierías - CIS, Universidad de Guanajuato"
+    }
+]
+
+
+# Materias impartidas
+materias = [
+    "Cálculo diferencial e integral",
+    "Álgebra lineal",
+    "Estática",
+    "Taller de Diseño",
+    "Mecánica de Sólidos I",
+    "Software para el dibujo industrial",
+    "Metodología del diseño e innovación",
+    "Dinámica",
+    "Fundamentos de Física",
+    "Mecanismos",
+    "Simulación y laboratorio industrial",
+    "Taller de diseño de productos de alta tecnología",
+    "Taller de manejo de maderas y textiles",
+    "Procesos industriales de metales y materiales de alta tecnología"
+]
+
+
+# Membresías
+membresia = [
+    {
+        'periodo': '2016-2019',  
+        'comite': 'Comité Sectorial de Análisis y Diseño Asistido por Computadora con clave (490)',  
+        'registro': 'Certificación de Competencias Laborales'  
+    },
+    {
+        'periodo': '2021-2024',  
+        'comite': 'Membresía de la Sociedad Mexicana de Ingeniería Mecánica (SOMIM)',  
+        'registro': 'Número de Registro 1185'  
+    }
+]
+
+
+# Patentes
+patentes = [
+    {
+        'anio': '2022',  
+        'titulo': 'Título: Cryogenic compression storage using a hump-reinforced vacuum jacket',  
+        'inventores': 'Inventores: Aceves, S., Espinosa-Loza, F., Guillaume, P., Vernon, A. Switzer, Ledesma-Orozco, E., Alcántar-Camarena, V.',  
+        'num_patente': 'Número de patente: JP7184778B2',  
+        'pais': 'País: Japón',  
+        'enlace': 'https://patentimages.storage.googleapis.com/37/73/32/b427ed09ec4819/JP7184778B2.pdf'   
+    },
+    {
+        'anio': '2021',  
+        'titulo': 'Título: Cryogenic compression storage using a hump-reinforced vacuum jacket',  
+        'inventores': 'Inventores: Aceves, S., Espinosa-Loza, F., Guillaume, P., Vernon, A. Switzer, Ledesma-Orozco, E., Alcántar-Camarena, V.',  
+        'num_patente': 'Número de patente: US10928006B2',  
+        'pais': 'País: EE.UU.',  
+        'enlace': 'https://patentimages.storage.googleapis.com/c8/2b/8f/d0338863ac74e2/US10928006.pdf'   
+    },
+    {
+        'anio': '2018',  
+        'titulo': 'Título: Cryogenic compression storage using a hump-reinforced vacuum jacket',  
+        'inventores': 'Inventores: Aceves, S., Espinosa-Loza, F., Guillaume, P., Vernon, A. Switzer, Ledesma-Orozco, E., Alcántar-Camarena, V.',  
+        'num_patente': 'Número de patente: US10082246B2',  
+        'pais': 'País: EE.UU.',  
+        'enlace': 'https://patentimages.storage.googleapis.com/a9/fd/ed/77c8b6fa911da9/US10082246.pdf'   
+    }
+]
+
+
+# Publicaciones en revistas indizadas
+indizadas = [
+    {
+        'anio': '2025',  
+        'titulo': 'Título: "Review on the integration of high-temperature heat pumps in district heating and cooling networks"',  
+        'autores': 'Autores: Raya-González, L.E., Alcántar-Camarena, V., Saldaña-Robles, A., Duque-Vázquez, E.F., Tapia-Tinoco, G., Saldaña-Robles, N.',  
+        'journal': 'Journal: *Computers and Electronics in Agriculture*',  
+        'volumen': 'Volumen: 235',  
+        'paginas': 'Páginas: 110375',  
+        'revista': 'Revista: Elsevier',  
+        'doi': 'DOI: https://doi.org/10.1016/j.compag.2025.110375',  
+        'impacto': 'Factor de impacto: 8.9'  
+    },
+    {
+        'anio': '2022',  
+        'titulo': 'Título: "Review on the integration of high-temperature heat pumps in district heating and cooling networks"',  
+        'autores': 'Autores: Barco-Burgos, J., Carles-Bruno, J., Eicker, U., Saldaña-Robles, A.L., Alcántar-Camarena, V.',  
+        'journal': 'Journal: *Energy*',  
+        'volumen': 'Volumen: 239',  
+        'paginas': 'Páginas: 122378',  
+        'revista': 'Revista: Elsevier',  
+        'doi': 'DOI: https://doi.org/10.1016/j.energy.2021.122378',  
+        'impacto': 'Factor de impacto: 9.4'  
+    },
+    {
+        'anio': '2021',  
+        'titulo': 'Título: "Hydrogen-rich syngas production from palm kernel shells (PKS) biomass on a downdraft allothermal gasifier using steam as a gasifying agent"',  
+        'autores': 'Autores: Barco-Burgos, J., Carles-Bruno, J., Eicker, U., Saldaña-Robles, A.L., Alcántar-Camarena, V.',  
+        'journal': 'Journal: *Energy Conversion and Management*',  
+        'volumen': 'Volumen: 245',  
+        'paginas': 'Páginas: 114592',  
+        'revista': 'Revista: Elsevier',  
+        'doi': 'DOI: https://doi.org/10.1016/j.enconman.2021.114592',  
+        'impacto': 'Factor de impacto: 10.9'  
+    },
+    {
+        'anio': '2021',  
+        'titulo': 'Título: "Effect of the ball burnishing on the surface quality and mechanical properties of a TRIP steel sheet"',  
+        'autores': 'Autores: Capilla-González, G., Martínez-Ramírez, I., Díaz-Infante, D., Hernández-Rodríguez, E., Alcántar-Camarena, V., Saldaña-Robles, A.',  
+        'journal': 'Journal: *The International Journal of Advanced Manufacturing Technology*',  
+        'volumen': 'Volumen: 116, No. 11',  
+        'paginas': 'Páginas: 3953-3964',  
+        'revista': 'Revista: Springer',  
+        'doi': 'DOI: https://doi.org/10.1007/s00170-021-07715-x',  
+        'impacto': 'Factor de impacto: 3.1'  
+    },
+    {
+        'anio': '2020',  
+        'titulo': 'Título: "Thermal characterization of an alkaline electrolysis cell for hydrogen production at atmospheric pressure"',  
+        'autores': 'Autores: Barco-Burgos, J., Eicker, U., Saldaña-Robles, N., Saldaña-Robles, A.L., Alcántar-Camarena, V.',  
+        'journal': 'Journal: *Fuel*',  
+        'volumen': 'Volumen: 276',  
+        'paginas': 'Páginas: 117910',  
+        'revista': 'Revista: Elsevier',  
+        'doi': 'DOI: https://doi.org/10.1016/j.fuel.2020.117910',  
+        'impacto': 'Factor de impacto: 7.5'  
+    },
+    {
+        'anio': '2020',  
+        'titulo': 'Título: "Structural design of an agricultural backhoe using TA, FEA, RSM and ANN"',  
+        'autores': 'Autores: Saldaña-Robles, A.L., Bustos-Gaytán, A., Diosdado-De la Peña, J.A., Saldaña-Robles, A., Alcántar-Camarena, V., Balvantín-García, A., Saldaña-Robles, N.',  
+        'journal': 'Journal: *Computers and Electronics in Agriculture*',  
+        'volumen': 'Volumen: 172',  
+        'paginas': 'Páginas: 105278',  
+        'revista': 'Revista: Elsevier',  
+        'doi': 'DOI: https://doi.org/10.1016/j.compag.2020.105278',  
+        'impacto': 'Factor de impacto: 8.9'  
+    },
+    {
+        'anio': '2017',  
+        'titulo': 'Título: "Optimization of type III pressure vessels using genetic algorithm and simulated annealing"',  
+        'autores': 'Autores: Alcántar, V., Ledesma S., Aceves, S.M., Ledesma, E., Saldaña, A.',  
+        'journal': 'Journal: *International Journal of Hydrogen Energy*',  
+        'volumen': 'Volumen: 42, No. 31',  
+        'paginas': 'Páginas: 20125-20132',  
+        'revista': 'Revista: Elsevier',  
+        'doi': 'DOI: https://doi.org/10.1016/j.ijhydene.2017.06.146',  
+        'impacto': 'Factor de impacto: 8.3'  
+    },
+    {
+        'anio': '2017',  
+        'titulo': 'Título: "Optimization of Type 4 composite pressure vessels using genetic algorithms and simulated annealing"',  
+        'autores': 'Autores: Alcántar, V., Aceves, S., Ledesma, E., Ledesma, S., Aguilera, E.',  
+        'journal': 'Journal: *International Journal of Hydrogen Energy*',  
+        'volumen': 'Volumen: 42, No. 24',  
+        'paginas': 'Páginas: 15770-15781',  
+        'revista': 'Revista: Elsevier',  
+        'doi': 'DOI: https://doi.org/10.1016/j.ijhydene.2017.03.032',  
+        'impacto': 'Factor de impacto: 8.3'  
+    }
+]
+
